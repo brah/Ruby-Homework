1afa5ff64402f4debfda1f1b44fdc15088273c0d
qbabin <q.babin@gmail.com>
2014-08-03 21:04:44 -0700

    adding solutions

---
+++ gem-stones_solution.rb
@@ -0,0 +1,12 @@
+def gems comps
+  s = comps[0]
+  comps.each { |comp| s &= comp }
+  s.length
+end
+
+comps = []
+gets.chomp.to_i.times do
+  comps.push(Set.new(gets.chomp.scan(/\w/)))
+end
+
+puts gems comps
